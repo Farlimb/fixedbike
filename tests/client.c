2cf34d374aa7be8b4bcccb7d66df05d01ca06e26
Filip <filip.kusnir.2@student.tuke.sk>
2025-01-23 10:04:10 +0100

    commit

--- tests/client.c
+++ tests/client.c
@@ -97,7 +97,7 @@ void init_client(void) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "172.27.240.1", &serv_addr.sin_addr) <= 0) {
         perror("Invalid address");
         exit(EXIT_FAILURE);
     }
